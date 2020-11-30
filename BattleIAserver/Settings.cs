df5e573b3c2b4047559b9b92fc7c5c2d86ee6115
Sylvain Taverniers <55266056+callme-sy@users.noreply.github.com>
2020-11-30 10:24:33 +0100

    Update Settings.cs
    ajout de commentaires

--- BattleIAserver/Settings.cs
+++ BattleIAserver/Settings.cs
@@ -5,27 +5,34 @@ namespace BattleIAserver
     public class Settings
     {
         public int ServerPort = 4626;
+        //le port qui sera utilisé par le serveur
         public int DelayBetweenEachBotTurn = 100;
+        //le délai en secondes entre chaque tour des bots
         public UInt16 MapWidth = 30;
         public UInt16 MapHeight = 24;
         public UInt16 MapPercentWall = 4;
         public UInt16 MapPercentEnergy = 5;
+        //les tailles de la map
 
         public UInt16 MaxDelaySecondByTurn = 5;
+        //le délai max par tour
 
         public UInt16 EnergyPodFrom = 1;
         public UInt16 EnergyPodTo = 50;
         public UInt16 EnergyPodLessEvery = 5;
         public UInt16 EnergyPodLessValue = 1;
         public UInt16 EnergyPodMin = 10;
+        //valeurs energie pods
 
         public bool EndlessMode = true;
         public bool autoRespawn = true;
+        //respawn automatique ou non
         [Newtonsoft.Json.JsonIgnoreAttribute]
         public UInt16 EnergyPodMax;
 
         public UInt16 EnergyCloakCostMultiplier = 2;
         public UInt16 EnergyLostByCloak = 5;
+        //cout en energie pour cacher
 
         public UInt16 EnergyStart = 100;
         public UInt16 EnergyLostByTurn = 1;
@@ -34,12 +41,15 @@ namespace BattleIAserver
         public UInt16 EnergyLostShot = 2;
         public UInt16 EnergyLostContactWall = 5;
         public UInt16 EnergyLostContactEnemy = 30;
+        //couts en energie
 
         public UInt16 PointByTurn = 1;
         public UInt16 PointByEnergyFound = 8;
         public UInt16 PointByEnnemyTouch = 80;
         public UInt16 PointByEnnemyKill = 200;
+        //points gagnes par action
         public String MapName = "map_1.txt";
+        //map utilisée
 
     }
 }
